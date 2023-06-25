26afb9cfdd1efc08d703b9b129a996a2057ca121
Niraj <neeraz.pandey2058@gmail.com>
2023-06-25 10:53:27 +0545

    Made a landing page for student management.

--- Vyomika/Student/urls.py
+++ Vyomika/Student/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.management, name='management'),
+    path('faculty/', views.faculty, name='faculty'),
+    path('student/', views.studentMgmt, name = 'student')
 ]
--- Vyomika/Student/views.py
+++ Vyomika/Student/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def management(request):
     return render(request, 'Student/home.html')
+
+def faculty(request):
+    return render(request, 'Student/class.html')
+
+def studentMgmt(request):
+    return render(request, 'Student/stdmgmt.html')
